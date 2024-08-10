92dd64eca9fbc0a9b2eaa30e0e60252d3ff43cd4
Joris Hoevenberg <87002024+J-Hoef@users.noreply.github.com>
2024-08-10 14:54:39 +0200

    Always pretty print json, inject TodoContext via services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApp_Backend.Models;
+
 namespace ToDoApp_Backend
 {
     public class Program
@@ -19,13 +22,22 @@ namespace ToDoApp_Backend
 
         public static void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            AddDbContexts(services);
+            
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
         }
 
+        private static void AddDbContexts(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+        }
+        
         public static void Configure(WebApplication app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
