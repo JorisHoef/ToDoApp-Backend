105b9e087ad0807e789aca2998586f8a005be3ca
Joris <j.hoevenberg@recreate365.com>
2024-09-14 19:23:56 +0200

    add 8081 to urls

--- Program.cs
+++ Program.cs
@@ -2,94 +2,97 @@ using Microsoft.EntityFrameworkCore;
 using ToDoAppBackend.Models;
 using ToDoAppBackend.Services;
 
-namespace ToDoAppBackend
+namespace ToDoAppBackend;
+
+public class Program
 {
-    public class Program
+    private const string FALLBACK_ADDRESS = "http://localhost/";
+
+    public static void Main(string[] args)
     {
-        private const string FALLBACK_ADDRESS = "http://localhost/";
+        var builder = WebApplication.CreateBuilder(args);
 
-        public static void Main(string[] args)
+        var apiBaseUrl = Environment.GetEnvironmentVariable("API_SERVER") ?? $"{FALLBACK_ADDRESS}";
+        var apiUri = new Uri(apiBaseUrl);
+
+        ConfigureServices(builder.Services, apiUri);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        Configure(app, builder.Environment);
+
+        // Listen on both HTTP and HTTPS
+        app.Urls.Add("http://*:8080");
+        app.Urls.Add("https://*:8081");
+
+        app.Run();
+    }
+
+    public static void ConfigureServices(IServiceCollection services, Uri apiUri)
+    {
+        services.AddControllers().AddJsonOptions(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.JsonSerializerOptions.WriteIndented = true;
+        });
 
-            var apiBaseUrl = Environment.GetEnvironmentVariable("API_SERVER") ?? $"{FALLBACK_ADDRESS}";
-            var apiUri = new Uri(apiBaseUrl);
+        AddDbContexts(services);
 
-            ConfigureServices(builder.Services, apiUri);
+        services.AddTransient<LinkCreator>();
+        services.AddScoped<ITaskItemMessageResolver, TaskItemMessageResolver>();
 
-            var app = builder.Build();
+        // Configure API client
+        services.AddHttpClient("ApiClient", client =>
+        {
+            client.BaseAddress = apiUri;
+        });
 
-            // Configure the HTTP request pipeline.
-            Configure(app, builder.Environment);
+        services.AddSingleton(apiUri);
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
 
-            app.Run();
+    private static void AddDbContexts(IServiceCollection services)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION");
+
+        if (environment == "Local")
+        {
+            // Use In-Memory Database in Local mode
+            services.AddDbContext<TaskItemContext>(opt => opt.UseInMemoryDatabase("TodoList"));
         }
-
-        public static void ConfigureServices(IServiceCollection services, Uri apiUri)
+        else
         {
-            services.AddControllers().AddJsonOptions(options =>
+            // Use PostgreSQL in other environments
+            if (string.IsNullOrEmpty(connectionString))
             {
-                options.JsonSerializerOptions.WriteIndented = true;
+                throw new InvalidOperationException("DATABASE_CONNECTION environment variable is not set.");
+            }
+
+            services.AddDbContext<TaskItemContext>(opt => opt.UseNpgsql(connectionString));
+        }
+    }
+
+    public static void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            // Redirect root URL to Swagger
+            app.MapGet("/", context =>
+            {
+                context.Response.Redirect("/swagger");
+                return Task.CompletedTask;
             });
-
-            AddDbContexts(services);
-
-            services.AddTransient<LinkCreator>();
-            services.AddScoped<ITaskItemMessageResolver, TaskItemMessageResolver>();
-
-            // Configure API client
-            services.AddHttpClient("ApiClient", client =>
-            {
-                client.BaseAddress = apiUri;
-            });
-
-            services.AddSingleton(apiUri);
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
         }
 
-        private static void AddDbContexts(IServiceCollection services)
-        {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION");
+        app.UseHttpsRedirection();
 
-            if (environment == "Local")
-            {
-                // Use In-Memory Database in Local mode
-                services.AddDbContext<TaskItemContext>(opt => opt.UseInMemoryDatabase("TodoList"));
-            }
-            else
-            {
-                // Use PostgreSQL in other environments
-                if (string.IsNullOrEmpty(connectionString))
-                {
-                    throw new InvalidOperationException("DATABASE_CONNECTION environment variable is not set.");
-                }
+        app.UseAuthorization();
 
-                services.AddDbContext<TaskItemContext>(opt => opt.UseNpgsql(connectionString));
-            }
-        }
-
-        public static void Configure(WebApplication app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-
-                // Redirect root URL to Swagger
-                app.MapGet("/", context =>
-                {
-                    context.Response.Redirect("/swagger");
-                    return Task.CompletedTask;
-                });
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-        }
+        app.MapControllers();
     }
 }
