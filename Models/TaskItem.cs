48b2b92ba8e92d280c5f0e222456f4e1a4b57e7c
Joris <j.hoevenberg@recreate365.com>
2024-09-14 21:01:24 +0200

    Explicitly state json property values in taskItem

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,21 +1,41 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace ToDoAppBackend.Models
 {
     public class TaskItem
     {
         [Key]
+        [JsonProperty("id")]
         public long Id { get; set; }
+        
+        [JsonProperty("name")]
         public string? Name { get; set; }
+        
+        [JsonProperty("taskItemMessage")]
         public TaskItemMessage? TaskItemMessage { get; set; }
-        [JsonConverter(typeof(JsonStringEnumConverter))]
+        
+        [JsonProperty("taskDataState")]
+        [System.Text.Json.Serialization.JsonConverter(typeof(StringEnumConverter))]
         public TaskDataState TaskDataState { get; set; } = TaskDataState.OPEN;
+        
+        [JsonProperty("createdAt")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+        [JsonProperty("updatedAt")]
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        
+        [JsonProperty("completedAt")]
         public DateTime CompletedAt { get; set; }
+        
+        [JsonProperty("deadlineAt")]
         public DateTime DeadlineAt { get; set; }
+        
+        [JsonProperty("subTasks")]
         public ICollection<TaskItem>? SubTasks { get; set; } = null;
+        
+        [JsonProperty("parentTaskId")]
         public long? ParentTaskId { get; set; }
     }
 }
